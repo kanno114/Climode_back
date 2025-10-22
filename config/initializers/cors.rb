fd92383feb1dd3e775be68dfa90b9713ca1d7f95
kanno <k.takumi114@gmail.com>
2025-10-22 12:02:20 +0900

    fix: CORS設定にlocalhost:3000を追加

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,8 +1,9 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins [
-      "http://front:3000",
-      "https://climode-front.vercel.app"
+      "http://front:3000",        # Docker内部
+      "http://localhost:3000",    # ローカル開発
+      "https://climode-front.vercel.app"  # 本番環境
     ]
     resource "*",
       headers: :any,
