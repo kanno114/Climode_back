0c341cc9478233f733fa71e9b4728048acf1cef0
kanno <k.takumi114@gmail.com>
2026-02-10 20:25:56 +0900

    fix: User に user_concern_topics の dependent: :destroy を追加
    RSpec のユニークバリデーションテストで destroy_all 時に
    user_concern_topics の外部キー制約エラーが発生していたため修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
   has_many :user_identities, dependent: :destroy
   has_many :daily_logs, dependent: :destroy
   has_many :push_subscriptions, dependent: :destroy
+  has_many :user_concern_topics, dependent: :destroy
 
   validates :email, presence: true, uniqueness: true
   validates :name, presence: true, unless: :oauth_provider?
