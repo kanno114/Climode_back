6b1aae49becdf0cff9b9183ba88652db2784e6c8
kanno <k.takumi114@gmail.com>
2026-02-12 17:30:55 +0900

    feat: 関心ワードに category カラムを追加
    将来のカテゴリ表示用にDBカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2026_02_11_140000) do
+ActiveRecord::Schema[7.2].define(version: 2026_02_12_120000) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -23,6 +23,7 @@ ActiveRecord::Schema[7.2].define(version: 2026_02_11_140000) do
     t.boolean "active", default: true, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "category"
     t.index ["key"], name: "index_concern_topics_on_key", unique: true
   end
 
