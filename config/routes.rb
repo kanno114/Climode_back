42b815ef224040e953a41fbf690f37f453481663
kanno <k.takumi114@gmail.com>
2026-02-16 21:10:18 +0900

    feat: アカウント削除APIエンドポイントの追加
    DELETE /api/v1/users/:id でユーザー自身のアカウントと
    関連データを完全削除できるAPIを実装。
    パスワード認証ユーザーはパスワード再入力、
    OAuthユーザーはconfirmパラメータで本人確認を行う。

--- config/routes.rb
+++ config/routes.rb
@@ -33,7 +33,7 @@ Rails.application.routes.draw do
         end
       end
 
-      resources :users, only: [ :show, :update ] do
+      resources :users, only: [ :show, :update, :destroy ] do
         collection do
           get :default_prefecture
         end
